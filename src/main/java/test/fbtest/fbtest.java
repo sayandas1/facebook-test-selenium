4b1ab8a74d3f124e3046dacaefbd176f603c7199
Sayan Das <75417317+sayandas1@users.noreply.github.com>
2023-04-08 17:32:00 +0530

    Update fbtest.java

--- src/main/java/test/fbtest/fbtest.java
+++ src/main/java/test/fbtest/fbtest.java
@@ -17,14 +17,11 @@ public class fbtest {
 		ChromeDriver driver = new ChromeDriver(chromeOptions);
 		System.out.println("Hi,welcome to selenium testcases");
 		
-		driver.get("https:32.54.45.12:8085/addressbook");
+		driver.get("https://www.facebook.com/");
 		driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
 		driver.findElement(By.className("v-button")).click();
-		driver.findElement(By.id("gwt-uid-5")).sendKeys("sameer khan");
-		driver.findElement(By.id("gwt-uid-7")).sendKeys("123456");
-		driver.findElement(By.id("gwt-uid-9")).sendKeys("1111111111");
-		driver.findElement(By.id("gwt-uid-11")).sendKeys("abc@gmail.com");
-		driver.findElement(By.id("gwt-uid-13")).sendKeys("3/31/23,");
+		driver.findElement(By.id("email")).sendKeys("**Email**");
+		driver.findElement(By.id("pass")).sendKeys("**Password**");
 		driver.findElement(By.className("v-button-primary")).click();
 		System.out.println("TestCase Executed");
 		Thread.sleep(5000);
